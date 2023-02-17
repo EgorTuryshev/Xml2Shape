e98545f4c115a1c8b7d23bdef921a7cb0416139b
Daniil11ru <daniilmiroshnik11062001@gmail.com>
2023-02-17 23:49:20 +0300

    python xslt processor changed to cpp xslt processor

--- xslt_processor.cpp
+++ xslt_processor.cpp
@@ -5,19 +5,21 @@
 
 QString xslt_processor::cwd = "";
 
-QString xslt_processor::processXSLT(QString xmlPath, QString xsltPath) {
+QString xslt_processor::processXSLT(QString xmlPath, QString xsltPath)
+{
     QProcess p;
     QStringList params;
-    QFileInfo info ("../Xml2Shape/xslt_processor.py"); // Возможно, надо будет изменить в будущем
     QFileInfo xmlInfo (xslt_processor::cwd + xmlPath); // ?
     QFileInfo xsltInfo (xslt_processor::cwd + xsltPath); // ?
-    params << info.absoluteFilePath() << xmlInfo.absoluteFilePath() << xsltInfo.absoluteFilePath();
-    p.start("python", params);
+    QString program = "../Xml2Shape/xslt_processor_app/xslt_processor.exe";
+    params << xmlInfo.absoluteFilePath() << xsltInfo.absoluteFilePath();
+    p.start(program, params);
     p.waitForFinished();
     QString output(p.readAllStandardOutput());
     return output;
 }
 
-void xslt_processor::setcwd(QString path) {
+void xslt_processor::setcwd(QString path)
+{
     xslt_processor::cwd = path;
 }
---
+++ xslt_processor_app/main.cpp
@@ -0,0 +1,28 @@
+#include <QCoreApplication>
+#include <QXmlQuery>
+#include <QFile>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    if (argc < 3)
+    {
+        return 1;
+    }
+
+    QString out = "";
+    QXmlQuery query(QXmlQuery::XSLT20);
+    QFile xmlFile(argv[1]);
+    QFile xslFile(argv[2]);
+    xmlFile.open(QIODevice::ReadOnly);
+    xslFile.open(QIODevice::ReadOnly);
+    query.setFocus(&xmlFile);
+    query.setQuery(&xslFile);
+    query.evaluateTo(&out);
+    xmlFile.close();
+    xslFile.close();
+
+    std::cout << out.toStdString();
+}
