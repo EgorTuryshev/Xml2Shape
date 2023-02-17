aecf895e2ccbcaa36082944c9a75e2135acc773c
Daniil11ru <daniilmiroshnik11062001@gmail.com>
2023-02-17 14:34:14 +0300

    fixed null geometry

--- appcore.cpp
+++ appcore.cpp
@@ -6,14 +6,7 @@ Appcore::Appcore(QObject *parent) : QObject(parent)
 }
 void Appcore::test(){
     xslt_processor::setcwd("./");
-    QString processedXML_str = xslt_processor::processXSLT("../Xml2Shape/samples/big.xml", "../Xml2Shape/samples/kpt.xsl");
-    QFile processedXML_file("processed.xml");
-    processedXML_file.open(QIODevice::WriteOnly | QIODevice::Text);
-    QTextStream writeStream(&processedXML_file);
-    writeStream << processedXML_str;
-    processedXML_file.close();
-    qDebug(logDebug()) << "After writing processed xml";
-
+    QString processedXML_str = xslt_processor::processXSLT("../Xml2Shape/samples/test.xml", "../Xml2Shape/samples/kpt.xsl");
     xml_parser::setXML(processedXML_str);
     QString featureType = xml_parser::readFeatureType();
     xml_header header = xml_parser::readTypeHeader();
@@ -31,4 +24,9 @@ void Appcore::openLog()
 void Appcore::clearLog()
 {
     qWarning(clr()) << "Лог был очищен пользователем";
+}
+
+void Appcore::autoClearingLogChanged(CheckState newState)
+{
+
 }
